ce02ac511414adbb33968ccd99e2309238a8dd4c
Bakirovildar <bakirovildar2011@mail.ru>
2022-09-29 22:16:41 +0500

    added a form

--- src/shared/Content/AddingTasks/AddForm/AddForm.tsx
+++ src/shared/Content/AddingTasks/AddForm/AddForm.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import './addform.css'
+
 export function AddForm() {
   return (
       <div>
-
+          <form>
+              <input className='inpt' type="text" placeholder={'Название задачи'}/>
+              <button className='btn-add'>Добавить</button>
+          </form>
       </div>
   );
 }
