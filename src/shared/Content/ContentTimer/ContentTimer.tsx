20adc0672c5d46730e3358bf264d50e6abc28023
Bakirovildar <bakirovildar2011@mail.ru>
2022-09-29 22:26:07 +0500

    add header in ContentTimer.tsx

--- src/shared/Content/ContentTimer/ContentTimer.tsx
+++ src/shared/Content/ContentTimer/ContentTimer.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
-import styles from './contenttimer.css';
+import './contenttimer.css';
 
 export function ContentTimer() {
   return (
-      <div>Timer</div>
+      <div className='timer-container'>
+          <div className='timer-content'>
+              <div className='timer-content-header'>
+                  <div>Сверстать сайт</div>
+                  <span>Помидор 1</span>
+              </div>
+          </div>
+      </div>
   );
 }
