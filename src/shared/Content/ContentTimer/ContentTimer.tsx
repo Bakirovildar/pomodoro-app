82b436ae8e9f6d763a1d53a2016db0fd5a968a8a
Bakirovildar <bakirovildar2011@mail.ru>
2022-09-30 20:46:33 +0500

    component ContentTimer ready

--- src/shared/Content/ContentTimer/ContentTimer.tsx
+++ src/shared/Content/ContentTimer/ContentTimer.tsx
@@ -11,7 +11,18 @@ export function ContentTimer() {
                   <span>Помидор 1</span>
               </div>
               <div className='timer-content-timer-container'>
-                  <div className='timer'><span>25:00</span><IconAddTime /></div>
+                  <div className='timer'>
+                      <span>25:00</span>
+                      <IconAddTime />
+                  </div>
+                  <div className='timer-description'>
+                      <div>Задача 1 -</div>
+                      <span>Сверстать сайт</span>
+                  </div>
+                  <div className='timer-btns'>
+                      <div className='btn-start'>Старт</div>
+                      <div className='btn-stop'>Стоп</div>
+                  </div>
               </div>
           </div>
       </div>
