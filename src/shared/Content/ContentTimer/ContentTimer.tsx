bab8787515bbfcd686d0cc7b3008191811268c53
Bakirovildar <bakirovildar2011@mail.ru>
2022-09-30 20:27:01 +0500

    add IconAddTime, add timer

--- src/shared/Content/ContentTimer/ContentTimer.tsx
+++ src/shared/Content/ContentTimer/ContentTimer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './contenttimer.css';
+import {IconAddTime} from "../../../icons/IconAddTime";
 
 export function ContentTimer() {
   return (
@@ -8,6 +9,9 @@ export function ContentTimer() {
               <div className='timer-content-header'>
                   <div>Сверстать сайт</div>
                   <span>Помидор 1</span>
+              </div>
+              <div className='timer-content-timer-container'>
+                  <div className='timer'><span>25:00</span><IconAddTime /></div>
               </div>
           </div>
       </div>
