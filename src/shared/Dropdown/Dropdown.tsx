fea74a9170d2ebe2b77a9d7ce4a2e4d5bd50fc75
Bakirovildar <bakirovildar2011@mail.ru>
2022-10-01 13:10:09 +0500

    add close dropdown Ref

--- src/shared/Dropdown/Dropdown.tsx
+++ src/shared/Dropdown/Dropdown.tsx
@@ -1,4 +1,4 @@
-import React, {ReactNode, useState} from 'react';
+import React, {ReactNode, useEffect, useRef, useState} from 'react';
 import './dropdown.css';
 import {IconIncrease} from "../../icons/IconIncrease";
 import {IconDecreasy} from "../../icons/IconDecreasy";
@@ -6,27 +6,42 @@ import {IconEdit} from "../../icons/IconEdit";
 import {IconDelete} from "../../icons/IconDelete";
 
 interface IDropdown {
-  button: ReactNode
+    button: ReactNode
 }
 
 export function Dropdown({button}: IDropdown) {
-  const [isOpen, setIsOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(false)
+    const dropdownElement = useRef<HTMLDivElement>(null)
 
-  return (
-      <div className='dropdown-wrapper'>
-        <div onClick={()=> setIsOpen(!isOpen)}>
-          {button}
-        </div>
-        {
-          isOpen
-              ?<div className='dropdown-item'>
-                  <div className='dropdown-item-list'><IconIncrease /><span>Увеличить</span></div>
-                  <div className='dropdown-item-list'><IconDecreasy /><span>Уменьшить</span></div>
-                  <div className='dropdown-item-list'><IconEdit /><span>Редактировать</span></div>
-                  <div className='dropdown-item-list'><IconDelete /><span>Удалить</span></div>
-              </div>
-              : ''
+    useEffect(() => {
+        function clickHandler(event: MouseEvent) {
+            if (event.target instanceof Node && !dropdownElement.current?.contains(event.target)) {
+                setIsOpen(false)
+            }
         }
-      </div>
-  );
+
+        document.addEventListener('click', clickHandler)
+
+        return () => {
+            document.removeEventListener('click', clickHandler)
+        }
+    }, [isOpen])
+
+    return (
+        <div ref={dropdownElement} className='dropdown-wrapper'>
+            <div onClick={() => setIsOpen(!isOpen)}>
+                {button}
+            </div>
+            {
+                isOpen
+                    ? <div className='dropdown-item'>
+                        <div className='dropdown-item-list'><IconIncrease/><span>Увеличить</span></div>
+                        <div className='dropdown-item-list'><IconDecreasy/><span>Уменьшить</span></div>
+                        <div className='dropdown-item-list'><IconEdit/><span>Редактировать</span></div>
+                        <div className='dropdown-item-list'><IconDelete/><span>Удалить</span></div>
+                    </div>
+                    : ''
+            }
+        </div>
+    );
 }
