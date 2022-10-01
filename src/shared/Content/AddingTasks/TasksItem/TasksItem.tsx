5bdea73f7e3c66e7e9a88db132cb8baa50e0ca2e
Bakirovildar <bakirovildar2011@mail.ru>
2022-10-01 12:45:55 +0500

    add sumTime in component TasksItem

--- src/shared/Content/AddingTasks/TasksItem/TasksItem.tsx
+++ src/shared/Content/AddingTasks/TasksItem/TasksItem.tsx
@@ -4,15 +4,18 @@ import {Dropdown} from "../../../Dropdown";
 import {IconDropdown} from "../../../../icons/IconDropdownButton";
 
 export function TasksItem() {
-  return (
-      <div>
-        <div className='tasks-item-container'>
-            <div className='tasks-item-desc'>
-                <div className='tasks-item-num'>1</div>
-                <span>Сверстать сайт</span>
+    return (
+        <div>
+            <div className='tasks-item-container'>
+                <div className='tasks-item-desc'>
+                    <div className='tasks-item-num'>1</div>
+                    <span>Сверстать сайт</span>
+                </div>
+                <Dropdown button={<IconDropdown/>}/>
             </div>
-            <Dropdown button={<IconDropdown />} />
+            <div className='sumTime'>
+                25 мин
+            </div>
         </div>
-      </div>
-  );
+    );
 }
